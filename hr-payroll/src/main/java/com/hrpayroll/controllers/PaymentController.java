a1d7fa566b75e42e7c6a36648ad3013378f3bc18
Sávio Lopes <savio.lopes404@gmail.com>
2021-07-21 23:17:23 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/hrpayroll/controllers/PaymentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hrpayroll.business.PaymentBusiness;
 import com.hrpayroll.model.Payment;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -16,9 +17,15 @@ public class PaymentController {
 	
 	@Autowired private PaymentBusiness paymentBusiness;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Long days){
 		Payment payment = paymentBusiness.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Long days){
+		Payment payment = new Payment("Brann", 400.00, days);
 		return ResponseEntity.ok(payment);
 	}
 	
--- hr-worker/src/main/java/com/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/hrworker/controllers/WorkerController.java
@@ -34,6 +34,14 @@ public class WorkerController {
 	@GetMapping("buscar-por-id/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
+		/*
+		try {
+			Thread.sleep(3000L);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		return ResponseEntity.ok(repository.findById(id).get());
