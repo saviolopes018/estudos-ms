2483c37d264cfbc085213b052a9a87d6765bb89d
Sávio Lopes <savio.lopes404@gmail.com>
2021-07-24 23:30:40 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/hrworker/controllers/WorkerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hrworker.model.Worker;
 import com.hrworker.repository.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}") private String testConfig;
+	
 	@Autowired private Environment env;
 	
 	@Autowired private WorkerRepository repository;
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("config = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
