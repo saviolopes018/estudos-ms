1d5da88b4c02c795630a325752daa02073a3d558
Sávio Lopes <savio.lopes404@gmail.com>
2021-08-11 23:39:52 -0300

    User, Role, many to many association

--- hr-worker/src/main/java/com/hrworker/model/Worker.java
+++ hr-worker/src/main/java/com/hrworker/model/Worker.java
@@ -15,7 +15,7 @@ public class Worker implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY	)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double dailyIncome;
